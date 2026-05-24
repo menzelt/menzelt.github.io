e845a76348ccb93aee85a9c14d5042a9f4304c70
Timo Menzel <timomenzel97@gmx.de>
2026-05-24 23:25:40 +0200

    Adjust a few things.
    - Add newest publication
    
    - Change 'Résumé' button to 'Resume'
    
    - Adjust publication card rendering in publications section on index page

--- js/publications.js
+++ js/publications.js
@@ -21,12 +21,24 @@ fetch('../data/publications.json')
           const card = document.createElement('div');
           card.classList.add('publication-card');
 
+          // process the authors array to bold the my name
+          const targetName = "Timo Menzel";
+          if (pub.authors.includes(targetName + "*")) {
+              // if the name is followed by an asterisk, we want to keep it in the processed string
+              pub.authors = pub.authors.replace(targetName + "*", `<strong>${targetName}*</strong>`);
+          } else {
+              pub.authors = pub.authors.replace(targetName, `<strong>${targetName}</strong>`);
+          }
+
+          // add a tooltip to the asterisk if it exists
+          pub.authors = pub.authors.split('*').join(`<span class="asterisk-hover" title="Both authors contributed equally">*</span>`);
+
           card.innerHTML = `
             <h4>${pub.title}</h4>
             <p class="authors">${pub.authors}</p>
-            <p class="venue">${pub.venue}</p>
+            <p class="venue">${pub.venue}, ${pub.year} ${pub.note ? `(${pub.note})` : ''}</p>
             <div class="publication-buttons">
-              ${pub.url ? `<a href="${pub.url}" target="_blank" class="btn">Official Publication</a>` : ''}
+              ${pub.url ? `<a href="${pub.url}" target="_blank" class="btn">Publisher Site</a>` : ''}
               ${pub.project_page ? `<a href="${pub.project_page}" target="_blank" class="btn">Project Page</a>` : ''}
             </div>
           `;
